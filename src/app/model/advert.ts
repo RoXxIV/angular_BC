678ae4b97b126e7d2db38b52cff22de4ff64ad8d
Hermier Evan <evan.hermier@gmail.com>
2021-04-07 09:40:27 +0200

    add model advert

---
+++ src/app/model/advert.ts
@@ -0,0 +1,13 @@
+export interface Advert {
+  id: number;
+  released_at: Date;
+  description: string;
+  width: string;
+  length: string;
+  shape: string;
+  concave: string;
+  picturePath: string;
+  model: [];
+  brand: [];
+  skateshop: [];
+}
